614baa5c3f480e9feb490cd728f4cf80502420f8
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-06 14:13:20 -0800

    added various ways of including static libraries

---
+++ more_libraries/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+    // same as #include <time.h>
+    // for including C libraries, preface name with 'c' and drop ".h"
+
+#include "../someLib.h" // must actually exist, otherwise pre-processing will fail
+
+#include <string> // only included first time
+
+int main(){
+
+
+    return 0;
+}
