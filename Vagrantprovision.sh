67894f9fb131e4f6130f3089da824652d69181a8
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-23 07:00:16 -0800

    set some VM terminal defaults

--- Vagrantprovision.sh
+++ Vagrantprovision.sh
@@ -1,3 +1,7 @@
 apt-get update
 apt-get upgrade
 apt-get install -y g++
+echo -e "\n# Set default bash shortcuts to vi mode" >> ~/.bashrc
+echo "set -o vi" >> ~/.bashrc
+echo -e "\n# Set starting directory to C++ programs directory" >> ~/.bashrc
+echo "cd /vagrant/programs" >> ~/.bashrc
