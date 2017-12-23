4ebd8d20209e6b2601f1054a00b11e94a185109b
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-12-23 07:35:20 -0800

    Set default C++ compile standard

--- Vagrantprovision.sh
+++ Vagrantprovision.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+# Set terminal defaults
+TERM_STARTUP=/home/ubuntu/.bashrc
+echo -e "\n# Set default bash shortcuts to vi mode" >> $TERM_STARTUP
+echo "set -o vi" >> $TERM_STARTUP
+echo -e "\n# Set default g++ compile standard" >> $TERM_STARTUP
+echo "alias g++=\"g++ -std=c++14\"" >> $TERM_STARTUP
+echo -e "\n# Set starting directory to C++ programs directory" >> $TERM_STARTUP
+echo "cd /vagrant/programs" >> $TERM_STARTUP
+
+# Install required modules
 apt-get update
 apt-get upgrade
 apt-get install -y g++
-echo -e "\n# Set default bash shortcuts to vi mode" >> ~/.bashrc
-echo "set -o vi" >> ~/.bashrc
-echo -e "\n# Set starting directory to C++ programs directory" >> ~/.bashrc
-echo "cd /vagrant/programs" >> ~/.bashrc
